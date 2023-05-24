6c5d1a4028a5d5a3979145199b7f80fca80ef252
Santiago Ruiz <ruizsantiago2001@gmail.com>
2023-05-24 18:53:17 -0300

    restablecer

--- api.js
+++ api.js
@@ -2,14 +2,11 @@
 // npm install --save express
 
 const express = require('express');
-const serverless = require('serverless-http');
 
 const app = express(); // Genera una app con express, se puede crear mas de una
-const router = express.Router();
 
-router.get('/', (req,res)=> {
+ap.get('/', (req,res)=> {
     res.send({ hi: 'buddy' });
 });
 
-app.use('/.netlify/functions/api',router);
-module.exports.handler = serverless(app);
+app.listen(3000);
