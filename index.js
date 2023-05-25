cbcc42c0053c617778ed28d553bb4c226cad0742
Santiago Ruiz <ruizsantiago2001@gmail.com>
2023-05-24 21:28:48 -0300

    google auth

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // npm init
 // npm install --save express
 
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express(); // Genera una app con express, se puede crear mas de una
 
-app.get('/', (req,res)=> {
-    res.send({ hi: 'buddy' });
-});
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback",
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log('AT',accessToken);
+    console.log('RT',refreshToken);
+    console.log('PR',profile);
+  }
+));
+
+app.get('/', (req,res)=>{
+    res.send({ hello: "World!"});
+})
+app.get('/auth/google', passport.authenticate('google',{
+    scope: ['profile','email']
+}));
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
